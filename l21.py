5be88d2f1e5cac7e989b411397bf3b3412c74e86
daiyue <dypublic@gmail.com>
2013-05-19 02:10:02 +0800

    l21 unzip hidden file (format previous version)
    http://www.pythonchallenge.com/pc/hex/copper.html

--- l21.py
+++ l21.py
@@ -8,7 +8,6 @@ st=open('21_package.pack').read()
 log=''
 log_len=len(log)
 while True:
-<<<<<<< HEAD
     try: #zlib
         st=zlib.decompress(st)
         log+=' '
@@ -21,18 +20,4 @@ while True:
             st=st[::-1]
             print log[log_len:]
             log_len=len(log)
-=======
-    try: #zlib
-        st=zlib.decompress(st)
-        log+=' '
-    except:
-        try: #bzip2
-            st=bz2.decompress(st)
-            log+='#'
-        except: #reverse
-            if log_len==len(log): break
-            st=st[::-1]
-            print log[log_len:]
-            log_len=len(log)
->>>>>>> b69110a797f3503ae3cb3e3901ff5a257c464033
 open('21_package.unpack','wb').write(st)
