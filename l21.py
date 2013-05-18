23a0448b72bd6a80ea2cf8e6c578cdfa821dffe9
daiyue <dypublic@gmail.com>
2013-05-19 01:53:53 +0800

    l21 unzip the hidden file
    http://www.pythonchallenge.com/pc/hex/copper.html

---
+++ l21.py
@@ -0,0 +1,23 @@
+'''
+Created on 2013-5-19
+
+@author: GunBar
+'''
+import zlib,bz2
+st=open('21_package.pack').read()
+log=''
+log_len=len(log)
+while True:
+    try: #zlib
+        st=zlib.decompress(st)
+        log+=' '
+    except:
+        try: #bzip2
+            st=bz2.decompress(st)
+            log+='#'
+        except: #reverse
+            if log_len==len(log): break
+            st=st[::-1]
+            print log[log_len:]
+            log_len=len(log)
+open('21_package.unpack','wb').write(st)
